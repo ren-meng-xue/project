98e16c011fa0421051ea798bb5fd680c084d84cc
任孟雪 <renmengxue@jqrjl.com>
2023-06-02 10:45:50 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import {} from "vue"
+import {} from "vue";
 </script>
 
 <style lang="scss" scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from "vue"
-import App from "./App.vue"
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount("#app")
+createApp(App).mount("#app");
 
 const fn = () => {
-  console.log(123)
-}
-fn()
+  console.log(123);
+};
+fn();
