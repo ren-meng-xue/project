ff3ec9d85e0e96019147a7f32f6c2ede457ebb0a
任孟雪 <renmengxue@jqrjl.com>
2023-06-20 23:01:50 +0800

    update: 角色管理静态搭建

--- src/views/acl/role/index.vue
+++ src/views/acl/role/index.vue
@@ -1,9 +1,74 @@
 <template>
-  <div class="">角色管理</div>
+  <div>
+    <el-card>
+      <el-form :inline="true" class="form">
+        <el-form-item label="用户名">
+          <el-input placeholder="输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">搜索</el-button>
+          <el-button type="primary">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card style="margin: 10px 0">
+      <el-button type="primary" icon="Plus">添加角色</el-button>
+      <el-table border style="margin: 10px 0">
+        <el-table-column
+          type="index"
+          label="#"
+          align="center"
+        ></el-table-column>
+        <el-table-column label="Id" align="center"></el-table-column>
+        <el-table-column
+          label="职位名称"
+          align="center"
+          show-overflow-tooltip
+        ></el-table-column>
+        <el-table-column
+          label="创建时间"
+          align="center"
+          show-overflow-tooltip
+        ></el-table-column>
+        <el-table-column
+          label="更新时间"
+          align="center"
+          show-overflow-tooltip
+        ></el-table-column>
+        <el-table-column
+          label="操作"
+          width="260px"
+          align="center"
+        ></el-table-column>
+      </el-table>
+      <el-pagination
+        v-model:current-page="pageNo"
+        v-model:page-size="pageSize"
+        :page-sizes="[10, 20, 30, 40]"
+        :background="true"
+        layout="  prev, pager, next, jumper,->,sizes,total"
+        :total="total"
+      />
+      <!-- @size-change="handleSizeChange"
+        @current-change="handleCurrentChange" -->
+    </el-card>
+  </div>
 </template>
 
 <script setup lang="ts">
-import {} from "vue";
+import { ref } from 'vue'
+//当前页码
+let pageNo = ref<number>(1)
+//一页显示多少条数据
+let pageSize = ref<number>(10)
+let total = ref<number>(0)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.form {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+}
+</style>
--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -104,7 +104,7 @@
     <el-drawer v-model="drawer">
       <!-- 头部标题：将来文字内容应该是动态的 -->
       <template #header>
-        <h4>{{ userParams.id ? "修改用户" : "新增用户" }}</h4>
+        <h4>{{ userParams.id ? '修改用户' : '新增用户' }}</h4>
       </template>
       <!-- 身体部分 -->
       <template #default>
@@ -179,79 +179,79 @@
 </template>
 
 <script setup lang="ts">
-import useLayoutSettingStore from "@/store/modules/setting";
-import { reqUserInfo } from "@/api/acl/user";
-import { ref, onMounted, reactive, nextTick } from "vue";
+import useLayoutSettingStore from '@/store/modules/setting'
+import { reqUserInfo } from '@/api/acl/user'
+import { ref, onMounted, reactive, nextTick } from 'vue'
 import type {
   userInfoResponseData,
   User,
   AllRoleResponseData,
   ALLRole,
-  SetRoleData,
-} from "@/api/acl/user/type";
+  SetRoleData
+} from '@/api/acl/user/type'
 import {
   redAddOrUpdateUser,
   reqAllRole,
   reqSetUserRole,
   reqRemoveUser,
-  reqSelectUser,
-} from "@/api/acl/user";
-import { ElMessage } from "element-plus";
+  reqSelectUser
+} from '@/api/acl/user'
+import { ElMessage } from 'element-plus'
 //默认第一页
-let pageNo = ref<number>(1);
+let pageNo = ref<number>(1)
 //一页展示多少条数据
-let pageSize = ref<number>(5);
+let pageSize = ref<number>(5)
 //用户的整个数量
-let userArr = ref<User[]>([]);
-let total = ref<number>(0);
+let userArr = ref<User[]>([])
+let total = ref<number>(0)
 //控制抽屉显示与隐藏
-let drawer = ref<boolean>(false);
+let drawer = ref<boolean>(false)
 //控制分配角色抽屉显示与隐藏
-let drawer1 = ref<boolean>(false);
+let drawer1 = ref<boolean>(false)
 
 //收集用户信息的响应式数据
 let userParams = reactive<User>({
-  username: "",
-  name: "",
-  password: "",
-});
+  username: '',
+  name: '',
+  password: ''
+})
 
 //准备一个数组存储批量删除的用户ID
-let selectIdArr = ref<User[]>([]);
+let selectIdArr = ref<User[]>([])
 //获取form组件实例
-let formRef = ref<any>();
+let formRef = ref<any>()
 
 //定义响应式数据 ：收集用户输入进来的关键字
-let keywords = ref<string>("");
+let keywords = ref<string>('')
 
 //获取setting仓库模版
-let settingStore = useLayoutSettingStore();
+let settingStore = useLayoutSettingStore()
 //存储全部用户的数据
 onMounted(() => {
-  getHasUser();
-});
+  getHasUser()
+})
 
 //获取全部已有的用户信息
 const getHasUser = async (pager = 1) => {
-  pageNo.value = pager;
+  pageNo.value = pager
   let result: userInfoResponseData = await reqUserInfo(
     pageNo.value,
     pageSize.value,
     keywords.value
-  );
+  )
   if (result.code == 200) {
-    total.value = result.data.total;
-    userArr.value = result.data.records;
+    total.value = result.data.total
+    userArr.value = result.data.records
   }
-};
+}
 //每一页显示的条数
 // cosnt handleSizeChange = (num:number) =>{
 //   pageSize.value=num
 // }
 
 const handleSizeChange = () => {
-  getHasUser();
-};
+  getHasUser()
+}
 
 //添加用户按钮的回调
 const addUser = () => {
@@ -259,133 +259,137 @@ const addUser = () => {
   // 或者问号
   //清空一下数据
   Object.assign(userParams, {
-    username: "",
-    name: "",
-    password: "",
-    id: "",
-  });
+    username: '',
+    name: '',
+    password: '',
+    id: ''
+  })
 
   //抽屉显示出来
-  drawer.value = true;
+  drawer.value = true
   nextTick(() => {
     // log
-    formRef.value.clearValidate("password");
-    formRef.value.clearValidate("username");
-    formRef.value.clearValidate("name");
-  });
-};
+    formRef.value.clearValidate('password')
+    formRef.value.clearValidate('username')
+    formRef.value.clearValidate('name')
+  })
+}
 
 //更新已有的按钮的回调(row即为已有的用户信息)
 const updateUser = (row: User) => {
   //抽屉显示出来
-  drawer.value = true;
+  drawer.value = true
   //存储收集已有的账号信息
-  Object.assign(userParams, row);
+  Object.assign(userParams, row)
   nextTick(() => {
-    formRef.value.clearValidate("username");
-    formRef.value.clearValidate("name");
-  });
-};
+    formRef.value.clearValidate('username')
+    formRef.value.clearValidate('name')
+  })
+}
 //保存按钮的回调｜编辑
 const save = async () => {
   //务必需要保证表单全部符合条件再去发请求
-  console.log(formRef.value.validate());
-  await formRef.value.validate();
-  let result: any = await redAddOrUpdateUser(userParams);
+  console.log(formRef.value.validate())
+  await formRef.value.validate()
+  let result: any = await redAddOrUpdateUser(userParams)
   //添加或者更新成功
   if (result.code == 200) {
     //关闭抽屉
-    drawer.value = false;
+    drawer.value = false
     //提示消息
     ElMessage({
-      type: "success",
-      message: userParams.id ? "更新成功" : "添加成功",
-    });
+      type: 'success',
+      message: userParams.id ? '更新成功' : '添加成功'
+    })
     //获取全部已有的信息
     // 如果有id，停留在当前页面，否则是回到第一页
     // getHasUser(userParams.id ? pageNo.value : 1);
     //改了用户名，admin1，如果右上角还是admin，就不对，应该刷新浏览器就会自动跳转到登陆页面，在登陆
     // 所以需要浏览器自动刷新一次
-    window.location.reload();
+    window.location.reload()
   } else {
     //关闭抽屉
     //提示消息
     ElMessage({
-      type: "error",
-      message: userParams.id ? "更新失败" : "添加失败",
-    });
+      type: 'error',
+      message: userParams.id ? '更新失败' : '添加失败'
+    })
   }
-};
+}
 //取消按钮的回调
 const cancel = () => {
-  drawer.value = false;
-};
+  drawer.value = false
+}
 
 //校验用户名字回调函数
 // 如果校验规则用change，然后在清楚表单验证，清楚不掉，因为首先清空了
 const validatorUserName = (rule: any, value: any, callBack: any) => {
   // 用户名称、长度 至少5位
   if (value.trim().length >= 5) {
-    callBack();
+    callBack()
   } else {
-    callBack(new Error("用户名字｜昵称至少五位"));
+    callBack(new Error('用户名字｜昵称至少五位'))
   }
-};
+}
 //密码
 const validatorPassword = (rule: any, value: any, callBack: any) => {
   // 用户名称、长度 至少5位
   if (value.trim().length >= 6) {
-    callBack();
+    callBack()
   } else {
-    callBack(new Error("密码至少五位"));
+    callBack(new Error('密码至少五位'))
   }
-};
+}
 //表单校验规则对象
 const rules = {
   //用户名字
-  username: [{ required: true, trigger: "blur", validator: validatorUserName }],
+  username: [{ required: true, trigger: 'blur', validator: validatorUserName }],
   //用户名字
-  name: [{ required: true, trigger: "blur", validator: validatorUserName }],
+  name: [{ required: true, trigger: 'blur', validator: validatorUserName }],
   //用户的密码
-  password: [{ required: true, trigger: "blur", validator: validatorPassword }],
-};
+  password: [{ required: true, trigger: 'blur', validator: validatorPassword }]
+}
 
 //分配角色按钮回调
 const setRole = async (row: User) => {
   //存储已有用户信息
-  Object.assign(userParams, row);
-  let result: AllRoleResponseData = await reqAllRole(userParams.id as number);
-  console.log(result, "result");
+  Object.assign(userParams, row)
+  let result: AllRoleResponseData = await reqAllRole(userParams.id as number)
+  console.log(result, 'result')
   if (result.code == 200) {
     //存储全部的职位
-    allRole.value = result.data.allRolesList;
+    allRole.value = result.data.allRolesList
     //存储当前用户已有的职位
-    userRole.value = result.data.assignRoles;
+    userRole.value = result.data.assignRoles
     //抽屉i显示出
-    drawer1.value = true;
+    drawer1.value = true
+    let ids: string[] = userRole.value.map((item) => {
+      return item.id
+    })
+    handleCheckedCitiesChange(ids)
   }
 
   //发请求获取全部职位的数据，以及当前用户已有职位的数据
-};
+}
 //全选复选框收集数据：是否全选
-let checkAll = ref(false);
+let checkAll = ref(false)
 //设置不确定状态，仅负责样式控制
-let isIndeterminate = ref(true);
-let userRole = ref<ALLRole>([]);
-let allRole = ref<ALLRole>([]);
+let isIndeterminate = ref(true)
+let userRole = ref<ALLRole>([])
+let allRole = ref<ALLRole>([])
 //全选复选框的change事件
 const handleCheckAllChange = (val: boolean) => {
-  userRole.value = val ? allRole.value : [];
-  isIndeterminate.value = false;
-};
+  userRole.value = val ? allRole.value : []
+  isIndeterminate.value = false
+}
 //底部复选框change
 const handleCheckedCitiesChange = (value: string[]) => {
   //已经够选的这些项目的长度
-  const checkedCount = value.length;
-  checkAll.value = checkedCount === allRole.value.length;
+  const checkedCount = value.length
+  checkAll.value = checkedCount === allRole.value.length
   //顶部的复选框不确定的样式
-  isIndeterminate.value = !(checkedCount === allRole.value.length);
-};
+  isIndeterminate.value = !(checkedCount === allRole.value.length)
+}
 //分配职位确定按钮回调
 
 const confirmClick = async () => {
@@ -393,73 +397,73 @@ const confirmClick = async () => {
   let data: SetRoleData = {
     userId: userParams.id as number,
     roleIdList: userRole.value.map((item) => {
-      return item.id as number;
-    }),
-  };
+      return item.id as number
+    })
+  }
   //分配用户的职位
-  let result: any = await reqSetUserRole(data);
-  console.log(result, "89");
+  let result: any = await reqSetUserRole(data)
+  console.log(result, '89')
   if (result.code == 200) {
     ElMessage({
-      type: "success",
-      message: "分配职务成功",
-    });
+      type: 'success',
+      message: '分配职务成功'
+    })
     //关闭抽屉
-    drawer1.value = false;
+    drawer1.value = false
     //更新完毕留在当前页
-    getHasUser(pageNo.value);
+    getHasUser(pageNo.value)
   }
-};
+}
 
 //删除某一个用户
 const deleteUser = async (userId: number) => {
-  let result: any = await reqRemoveUser(userId);
+  let result: any = await reqRemoveUser(userId)
   if (result.code == 200) {
     ElMessage({
-      type: "success",
-      message: "删除成功",
-    });
-    getHasUser(userArr.value.length > 1 ? pageNo.value : pageNo.value - 1);
+      type: 'success',
+      message: '删除成功'
+    })
+    getHasUser(userArr.value.length > 1 ? pageNo.value : pageNo.value - 1)
   }
-};
+}
 
 //table复选框勾选的时候触发的事件
 const selectChange = (val: any) => {
-  selectIdArr.value = val;
-};
+  selectIdArr.value = val
+}
 
 //批量删除按钮的回调
 const deleteSelectUser = async () => {
   //整理批量删除的参数
   let idList = selectIdArr.value.map((item) => {
-    return item.id as number;
-  });
-  let result: any = await reqSelectUser(idList);
+    return item.id as number
+  })
+  let result: any = await reqSelectUser(idList)
   if (result.code == 200) {
     ElMessage({
-      type: "success",
-      message: "删除成功",
-    });
-    getHasUser(userArr.value.length > 1 ? pageNo.value : pageNo.value - 1);
+      type: 'success',
+      message: '删除成功'
+    })
+    getHasUser(userArr.value.length > 1 ? pageNo.value : pageNo.value - 1)
   } else {
     ElMessage({
-      type: "error",
-      message: result.data,
-    });
+      type: 'error',
+      message: result.data
+    })
   }
-};
+}
 //搜索按钮的回调
 const search = () => {
   //根据关键字获取相应的用户数据
-  getHasUser();
+  getHasUser()
   //清空关键字
-  keywords.value = "";
-};
+  keywords.value = ''
+}
 
 //重置按钮的回调
 const reset = () => {
-  settingStore.refresh = !settingStore.refresh;
-};
+  settingStore.refresh = !settingStore.refresh
+}
 </script>
 
 <style lang="scss" scoped>
